ec50a1304ffcd1a7fd9cb8c1a892fb8519ab9003
Godoflaugh <lam.kevin91@gmail.com>
2022-05-30 15:03:21 -0700

    Added click searching functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
 // Variable Declaration
-var inputValue = document.querySelector('.cityInput')
-var searchBtn = document.getElementById('#searchBtn')
+var inputEl
+var searchEl 
 
-
-
+ let pastCities  = []
 
 
 // This will be the object of weather that 1st. calls the api that will return lat and lon data that needs to be passed into the second fetch request that has a database that includes a 5 day forecast. 2. Use the data from the 2nd database to pass weather data into the HTML.
@@ -27,23 +26,62 @@ let weather = {
                     + this.apiKey)
             })
             .then(response => response.json())
-            .then(weatherData => console.log(weatherData))
+            .then(weatherData => this.displaylatlon(weatherData))
 
     },
-    // Function to grab the latitude and longitude of the city
-    //  displaylatlon: function(weatherData) {
-    //     const {lat, lon} = weatherData.coord
-    //     const{temp} = weatherData.main
 
-    //     return lat, lon
-    //     console.log(lat, lon, temp)
-    //  }
+    displaylatlon: function (weatherData) {
+        const { timezone } = weatherData
+        const { temp, humidity, uvi, wind_speed } = weatherData.current
+        const { main, description, icon } = weatherData.current.weather[0]
+        const { sunrise } = weatherData.daily[1]
 
 
+        document.getElementById("temperature").innerText = "Temperature: " + temp + " F"
+        document.getElementById("humidity").innerText = "Humidity: " + humidity + " %"
+        document.getElementById("wind").innerText = "Wind Speed: " + wind_speed + " MPH"
+        document.getElementById("UV").innerText = "UVI: " + uvi
+    },
+
+    search: function () {
+        //  Need to implement taking input from form, clicking will start search, then a function to handle the enter key being presssed to start search
+
+    }
+
+        
 }
 
-// Add button listener to the submit button, as well as taking in input from the input section in the HTML for the searching function.
+// Allows users to search for city by hitting button
+document.getElementById("searchBtn").onclick = function(){
 
+    event.preventDefault()
+    var inputEl = document.getElementById("cityName").value
+
+    weather.fetchWeather(inputEl)
+}
+
+function oldCities(){
+    const storedCities = JSON.parse(localStorage.getItem('pastCities'))
+    if(storedCities){
+        pastCities = storedCities
+    }
+}
+
+function storeCities(){
+    localStorage.setItem('pastCities', JSON.stringify('pastCities'))
+}
+
+
+// asdfas
+
+// document.getElementById("searchBtn").onclick = function () {
+    
+//     let inputEl = document.getElementById("cityName").value
+//     console.log(inputEl)
+//     return inputEl
+// }
+
+// Add button listener to the submit button, as well as taking in input from the input section in the HTML for the searching function.
 
 
 
